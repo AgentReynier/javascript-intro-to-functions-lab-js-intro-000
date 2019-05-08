4824dcbab41de06a806bfa53318da46d63823889
Corey Chriss <excelsior89@gmail.com>
2019-05-08 20:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  else if (uppercase.toUpperCase() === uppercase) {
+  if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!"
   }
 
