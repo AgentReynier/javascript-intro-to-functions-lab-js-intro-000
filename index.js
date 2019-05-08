9695ba404295ba9f803cf28f46f6ae093fd85244
Corey Chriss <excelsior89@gmail.com>
2019-05-08 20:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ function sayHiToGrandma(string) {
   var iLoveYou = "I love you, Grandma."
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!"
+
   }
+
   else if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!"
+
   }
+
 }
